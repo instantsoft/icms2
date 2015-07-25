a3f611de14285b2ddb15cb3de5465c16eddcb89b
lokanaft <lokanaft@gmail.com>
2015-07-25 22:29:12 +0300

    Как вариант выноса кода из шаблонов

--- templates/default/assets/fields/listgroups.tpl.php
+++ templates/default/assets/fields/listgroups.tpl.php
@@ -1,2 +1,2 @@
 <?php if ($field->title) { ?><label for="<?php echo $field->id; ?>"><?php echo $field->title; ?></label><?php } ?>
-<?php echo html_select_multiple($field->element_name, $field->dat['groups'], $value, array('id'=>$field->id)); ?>
+<?php echo html_select_multiple($field->element_name, $field->data['groups'], $value, array('id'=>$field->id)); ?>
