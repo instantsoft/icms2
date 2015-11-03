fc3f865dcdd1563db2513b48877241c9dbf353a5
mafru <mafru-84@mail.ru>
2015-11-03 15:43:36 +0300

    Update menu.php

--- system/languages/en/widgets/menu.php
+++ system/languages/en/widgets/menu.php
@@ -4,4 +4,4 @@
     define('LANG_WD_MENU_MAX_ITEMS',        'Max number of items to show');
     define('LANG_WD_MENU_MAX_ITEMS_HINT',   'The remaining items will be hidden under the &laquo;More...&raquo;<br>0 &mdash; unlimited');
     define('LANG_WD_MENU_TEMPLATE_NAME',     'The name of the menu template file');
-	define('LANG_WD_MENU_TEMPLATE_NAME_HINT', 'The template file is located the folder <b>widgets/menu</b> of your template, without the extension <b>.tpl.php</b>');
+    define('LANG_WD_MENU_TEMPLATE_NAME_HINT', 'The template file is located the folder <b>widgets/menu</b> of your template, without the extension <b>.tpl.php</b>');
