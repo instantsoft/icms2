d2b255515c1d3aa8588dda00075b7bca01e6de50
mafru <mafru-84@mail.ru>
2015-11-03 15:44:17 +0300

    Update menu.php

--- system/languages/ru/widgets/menu.php
+++ system/languages/ru/widgets/menu.php
@@ -4,4 +4,4 @@
     define('LANG_WD_MENU_MAX_ITEMS',        'Максимальное количество пунктов');
     define('LANG_WD_MENU_MAX_ITEMS_HINT',   'Остальные пункты будут помещены в пункт &laquo;Еще...&raquo;<br>0 &mdash; бесконечное количество');
     define('LANG_WD_MENU_TEMPLATE_NAME',     'Название файла шаблона для меню');
-	define('LANG_WD_MENU_TEMPLATE_NAME_HINT', 'Файл шаблона размещается папке <b>widgets/menu</b> вашего шаблона, без расширения <b>.tpl.php</b>');
+    define('LANG_WD_MENU_TEMPLATE_NAME_HINT', 'Файл шаблона размещается папке <b>widgets/menu</b> вашего шаблона, без расширения <b>.tpl.php</b>');
