7ed0359e269334daa9e5c9d937b68195622c4d73
fuze <fuze@localhost>
2015-12-10 10:37:20 +0300

    В файлах сайтмапов в одном файле не более 45000 записей. если записей больше, будет разделение по указанному кол-ву

--- system/controllers/sitemap/hooks/cron_generate.php
+++ system/controllers/sitemap/hooks/cron_generate.php
@@ -2,20 +2,25 @@
 
 class onSitemapCronGenerate extends cmsAction {
 
+    // вообще ограничение 50000 или 10Мб, чтобы не проверять размер файла, задаем меньше
+    private $max_count = 45000;
+
     public function run(){
 
         // автоматическое получение опций через $this->options здесь не
         // работает, потому что форма опций не содержит полей, они заполняются
         // динамически в админке
         $options = $this->loadOptions($this->name);
-
         if (!$options) { return false; }
 
         $sources_list = $options['sources'];
-
         if (!$sources_list) { return false; }
 
         $config = cmsConfig::getInstance();
+
+        if(!is_writable($config->root_path.'cache/static/sitemaps/')){
+            return false;
+        }
 
         $sources = array();
         $sitemaps = array();
@@ -35,18 +40,41 @@ class onSitemapCronGenerate extends cmsAction {
             foreach($items as $item){
 
                 $urls = $controller->runHook('sitemap_urls', array($item));
-				
 				if (!$urls) { continue; }
 
-                $xml = cmsTemplate::getInstance()->renderInternal($this, 'sitemap', array(
-                    'urls' => $urls
-                ));
+                if(count($urls)>$this->max_count){
 
-                $sitemap_file = "sitemap_{$controller_name}_{$item}.xml";
+                    $chunk_data = array_chunk($urls, $this->max_count, true);
 
-                file_put_contents($config->root_path . "cache/static/sitemaps/{$sitemap_file}", $xml);
+                    foreach ($chunk_data as $index=>$chunk_urls) {
 
-                $sitemaps[] = $sitemap_file;
+                        $index = $index ? '_'.$index : '';
+
+                        $xml = cmsTemplate::getInstance()->renderInternal($this, 'sitemap', array(
+                            'urls' => $chunk_urls
+                        ));
+
+                        $sitemap_file = "sitemap_{$controller_name}_{$item}{$index}.xml";
+
+                        file_put_contents($config->root_path . "cache/static/sitemaps/{$sitemap_file}", $xml);
+
+                        $sitemaps[] = $sitemap_file;
+
+                    }
+
+                } else {
+
+                    $xml = cmsTemplate::getInstance()->renderInternal($this, 'sitemap', array(
+                        'urls' => $urls
+                    ));
+
+                    $sitemap_file = "sitemap_{$controller_name}_{$item}.xml";
+
+                    file_put_contents($config->root_path . "cache/static/sitemaps/{$sitemap_file}", $xml);
+
+                    $sitemaps[] = $sitemap_file;
+
+                }
 
             }
 
