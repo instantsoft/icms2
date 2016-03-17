3888340e70e1373fe9dcbcd599542f46bdfa72de
mafru <mafru-84@mail.ru>
2016-03-17 14:55:06 +0300

    Create publish_delayed_content.php

---
+++ system/controllers/activity/hooks/publish_delayed_content.php
@@ -0,0 +1,27 @@
+<?php
+class onActivityPublishDelayedContent extends cmsAction {
+
+    public function run($data){	
+	
+		foreach($data as $ctype_name => $items){
+		
+			foreach($items as $item){
+
+			$this->addEntry('content', "add.{$ctype_name}", array(
+				'user_id'         	=> $item['user_id'],
+				'subject_title' 	=> $item['title'],
+				'subject_id' 		=> $item['id'],
+				'subject_url' 		=> href_to($ctype_name, $item['slug'] . '.html'),
+				'is_private' 		=> isset($item['is_private']) ? $item['is_private'] : 0,
+				'group_id' 			=> isset($item['parent_id']) ? $item['parent_id'] : null,
+				'is_parent_hidden' 	=> $item['is_parent_hidden'],
+				'is_pub' 			=> true
+			));
+			
+			}
+		}
+        return $data;
+
+    }
+
+}
