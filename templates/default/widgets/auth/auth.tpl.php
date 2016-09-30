2baaf9f86549477d576ca807704a8884bf7db859
askras <askrasnikov@gmail.com>
2016-09-30 21:46:17 +0300

    Добавлен параметр tabindex для формы авторизации, для удобства пользователей. Исправлен предыдущий коммит

--- templates/default/widgets/auth/auth.tpl.php
+++ templates/default/widgets/auth/auth.tpl.php
@@ -3,25 +3,25 @@
 
         <div class="field">
             <label><?php echo LANG_EMAIL; ?>:</label>
-            <a href="<?php echo href_to('auth', 'register'); ?>"><?php echo LANG_REGISTRATION; ?></a>
+            <a href="<?php echo href_to('auth', 'register'); ?>" tabindex="6"><?php echo LANG_REGISTRATION; ?></a>
             <?php echo html_input('text', 'login_email', '', array('required'=>true, 'autofocus' => true, 'tabindex' => '1')); ?>
         </div>
 
         <div class="field">
             <label><?php echo LANG_PASSWORD; ?>:</label>
-            <a href="<?php echo href_to('auth', 'restore'); ?>"><?php echo LANG_FORGOT_PASS; ?></a>
+            <a href="<?php echo href_to('auth', 'restore'); ?>" tabindex="5"><?php echo LANG_FORGOT_PASS; ?></a>
             <?php echo html_input('password', 'login_password', '', array('required'=>true, 'tabindex' => '2')); ?>
         </div>
 
         <div class="options">
-            <input type="checkbox" id="remember" name="remember" value="1" tabindex="4" />
+            <input type="checkbox" id="remember" name="remember" value="1" tabindex="3" />
             <label for="remember">
                 <?php echo LANG_REMEMBER_ME; ?>
             </label>
         </div>
 
         <div class="buttons">
-            <?php echo html_submit(LANG_LOG_IN, LANG_LOG_IN, array('tabindex' => '3')); ?>
+            <?php echo html_submit(LANG_LOG_IN, LANG_LOG_IN, array('tabindex' => '4')); ?>
         </div>
 
     </form>
