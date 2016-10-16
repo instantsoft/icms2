a096011e7b2dc928803b2e9d76eaf2080ac4dcf4
Val <val.sfinx@gmail.com>
2016-10-16 03:41:51 +0300

    Update tags.php

--- system/languages/ru/controllers/tags/tags.php
+++ system/languages/ru/controllers/tags/tags.php
@@ -5,7 +5,7 @@
     define('LANG_TAGS_TAG',                 'Тег');
     define('LANG_TAGS_TAG_FREQUENCY',       'Частота');
 
-    define('LANG_TAGS_SEARCH_BY_TAG',       'Поиск по тегу «%s»');
+    define('LANG_TAGS_SEARCH_BY_TAG',       'Поиск по тегам: «%s»');
     define('LANG_TAGS_SEARCH_NO_RESULTS',   'Ничего не найдено');
 
     define('LANG_TAGS_TAG_DELETE_CONFIRM',  'Удалить тег «{tag}»?');
