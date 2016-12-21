c0b605b0444b5403800f97b50fa82f2f49f6c006
Fuze <fuze@instantcms.ru>
2016-12-21 12:48:11 +0300

    Показ пути к шаблону, если он не найден

--- system/core/template.php
+++ system/core/template.php
@@ -27,6 +27,7 @@ class cmsTemplate {
     protected $menus = array();
     protected $db_menus = array();
     protected $menu_loaded = false;
+    protected $not_found_tpls = array();
 
     protected $widgets = array();
     protected $widgets_group_index = 0;
@@ -1051,6 +1052,10 @@ class cmsTemplate {
             }
         }
 
+        if(!$exists){
+            $this->not_found_tpls[] = $file;
+        }
+
         return $exists;
 
     }
@@ -1095,7 +1100,8 @@ class cmsTemplate {
 
         if (!$tpl_file){
             if (!$is_check){
-                cmsCore::error(ERR_TEMPLATE_NOT_FOUND . ': ' . $tpl_file);
+                $last_not_found_tpl = end($this->not_found_tpls);
+                cmsCore::error(ERR_TEMPLATE_NOT_FOUND . ': ' . $this->site_config->root.$last_not_found_tpl);
             } else {
                 return false;
             }
