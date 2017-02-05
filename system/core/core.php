428ee19a69ebcb9779c2ee380bfa6d4d93bab0a2
WebManG <softas@ukr.net>
2017-02-05 19:22:48 +0200

    Вывод виджетов только в одной указанной позиции
    Опциональная возможность запускать виджеты только в одной указанной позиции для вывода в шаблонах списка и записи контента.

--- system/core/core.php
+++ system/core/core.php
@@ -733,8 +733,10 @@ class cmsCore {
 
     /**
      * Запускает все виджеты, привязанные к текущей странице
+     * или виджеты в указанной позиции
+     * @param string $position Имя позиции для обработки
      */
-    public function runWidgets(){
+    public function runWidgets($position = ''){
 
         // в админке нам виджеты не нужны
         if ($this->controller == 'admin') { return; }
@@ -754,7 +756,7 @@ class cmsCore {
                 if(!empty($widget['controller']) && !cmsController::enabled($widget['controller'])){
                     continue;
                 }
-                $this->runWidget($widget);
+                if (!$position || $widget['position'] == $position) { $this->runWidget($widget); }
             }
         }
 
