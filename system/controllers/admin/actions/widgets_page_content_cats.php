e8453a1d4e75bd24edaefcf8acd7824195be7e76
lokanaft <lokanaft@gmail.com>
2018-02-23 13:43:30 +0300

    Автоподбор урлов для масок страниц виджетов

---
+++ system/controllers/admin/actions/widgets_page_content_cats.php
@@ -0,0 +1,27 @@
+<?php
+
+class actionAdminWidgetsPageContentCats extends cmsAction {
+
+    public function run(){
+
+        if( !$this->request->isAjax()
+                ||
+            !($ctype_name = $this->request->get('value', ''))
+        ){cmsCore::error404();}
+
+        $content_model = cmsCore::getModel('content');
+        $ctype = $content_model->getContentTypeByName($ctype_name);
+        if(!$ctype){cmsCore::error404();}
+
+        $tree = $content_model->limit(0)->getCategoriesTree($ctype['name'], false)?:array();
+
+        $items = array();
+        foreach($tree as $item){
+            $items[(((cmsConfig::get('ctype_default') !== $ctype['name']) ? $ctype['name'].'/' : '').$item['slug'])] = str_repeat('- ', $item['ns_level']).' '.$item['title'];
+        }
+
+        return $this->cms_template->renderJSON($items);
+
+    }
+
+}
