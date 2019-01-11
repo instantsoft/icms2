2bf6c806a8beb09cdbcd70e47d7dbd77e09021aa
Fuze <fuze@instantcms.ru>
2019-01-11 12:51:26 +0300

    мелочи в css

--- templates/default/controllers/admin/content_grid_columns.tpl.php
+++ templates/default/controllers/admin/content_grid_columns.tpl.php
@@ -4,49 +4,54 @@
 <?php foreach($items as $type => $fields){ ?>
         <fieldset>
             <legend><?php echo constant('LANG_CP_GRID_COLYMNS_'.strtoupper($type)); ?></legend>
-            <table cellpadding="2" cellspacing="0" border="0" width="100%">
+            <table>
                 <tr>
-                    <th>&nbsp;</th>
                     <th><?php echo LANG_TITLE; ?></th>
                     <th><?php echo LANG_CP_GRID_COLYMNS_FILTER; ?></th>
                     <th><?php echo LANG_CP_GRID_COLYMNS_DISPLAY; ?></th>
                 </tr>
                 <?php foreach($fields as $name => $field){ ?>
                     <tr>
-                        <td><?php echo html_checkbox('columns['.$type.']['.$name.'][enabled]', !empty($config[$type][$name]['enabled'])); ?></td>
                         <td>
-                            <label><?php html($field['title']); ?></label>
+                            <label>
+                            <?php echo html_checkbox('columns['.$type.']['.$name.'][enabled]', !empty($config[$type][$name]['enabled'])); ?>
+                            <?php html($field['title']); ?>
+                            </label>
                             <?php echo html_input('hidden', "columns[{$type}][{$name}][field]", $name); ?>
                         </td>
                         <td>
-                            <?php if(!empty($field['filters'])){
+                            <?php
+                            if(!empty($field['filters'])){
                                 $field['filters'] = array(''=>LANG_SELECT) + $field['filters'];
                                 $selected = !empty($config[$type][$name]['filter']) ? $config[$type][$name]['filter'] : '';
                                 echo html_select("columns[{$type}][{$name}][filter]", $field['filters'], $selected);
-                            }else{
+                            } else {
                                 echo '&mdash;';
                             } ?>
                         </td>
                         <td>
-                            <?php if(!empty($field['handlers'])){
+                            <?php
+                            if(!empty($field['handlers'])){
                                 $field['handlers'] = array(''=>LANG_SELECT) + $field['handlers'];
                                 $selected = !empty($config[$type][$name]['handler']) ? $config[$type][$name]['handler'] : '';
                                 echo html_select("columns[{$type}][{$name}][handler]", $field['handlers'], $selected);
-                            }else
+                            } else
                             if(!empty($field['handlers_only'])){
                                 echo $field['handlers_only'];
-                            }else{
+                            } else {
                                 echo '&mdash;';
                             }
-                ?>
+                            ?>
                         </td>
                     </tr>
                 <?php } ?>
             </table>
         </fieldset>
 <?php } ?>
-        <div style="text-align:right"><?php echo html_submit(LANG_APPLY); ?>
-            <span style="margin-left:15px"><?php echo html_button(LANG_CP_GRID_COLYMNS_RESET, 'reset', 'return contentGridColumnsResetSettings()'); ?></span></div>
+        <div class="buttons">
+            <?php echo html_submit(LANG_APPLY); ?>
+            <?php echo html_button(LANG_CP_GRID_COLYMNS_RESET, 'reset', 'return contentGridColumnsResetSettings()'); ?>
+        </div>
 
     </form>
 
