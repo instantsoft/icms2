e91ce2bf7533cbd3d77d90ce4eccc93649a018be
mafru <mafru-84@mail.ru>
2019-04-21 23:45:03 +0300

    При пустой ленте выводится ошибка
    После очистки ленты, в блоке выводится ошибка:  Invalid argument supplied for foreach() in

--- templates/default/controllers/activity/backend/admin_dashboard_block_activity.tpl.php
+++ templates/default/controllers/activity/backend/admin_dashboard_block_activity.tpl.php
@@ -1,26 +1,28 @@
 <?php $this->addJS('templates/default/js/jquery.scrollbar.js'); ?>
 <div id="activity_wrap">
     <div class="activity_wrap scrollbar-macosx">
-        <ul class="links adb_list">
-            <?php foreach ($items as $item) { ?>
-                <li>
-                    <a href="<?php echo href_to('users', $item['user']['id']); ?>">
-                        <?php html($item['user']['nickname']); ?>
-                    </a>
-                    <div>
-                        <?php if (!empty($item['images'][0]['src'])) { ?>
-                            <div class="image">
-                                <img src="<?php echo $item['images'][0]['src']; ?>">
-                            </div>
-                        <?php } ?>
-                        <?php echo $item['description']; ?>
-                    </div>
-                    <div class="date<?php if(!empty($item['is_new'])){ ?> highlight_new<?php } ?>">
-                        <?php echo $item['date_diff']; ?>
-                    </div>
-                </li>
-            <?php } ?>
-        </ul>
+        <?php if ($items){ ?>
+            <ul class="links adb_list">
+                <?php foreach ($items as $item) { ?>
+                    <li>
+                        <a href="<?php echo href_to('users', $item['user']['id']); ?>">
+                            <?php html($item['user']['nickname']); ?>
+                        </a>
+                        <div>
+                            <?php if (!empty($item['images'][0]['src'])) { ?>
+                                <div class="image">
+                                    <img src="<?php echo $item['images'][0]['src']; ?>">
+                                </div>
+                            <?php } ?>
+                            <?php echo $item['description']; ?>
+                        </div>
+                        <div class="date<?php if(!empty($item['is_new'])){ ?> highlight_new<?php } ?>">
+                            <?php echo $item['date_diff']; ?>
+                        </div>
+                    </li>
+                <?php } ?>
+            </ul>
+        <?php } else { echo LANG_LIST_EMPTY; } ?>
     </div>
 </div>
 <script>
