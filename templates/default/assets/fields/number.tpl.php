bed883c8d23dab346e09bca7f635ad261600055f
шэльдэ бердэ бельдэ <asnenne@yandex.ru>
2023-04-08 11:34:19 +0300

    Update number.tpl.php

--- templates/default/assets/fields/number.tpl.php
+++ templates/default/assets/fields/number.tpl.php
@@ -1,4 +1,4 @@
 <?php if ($field->title) { ?><label for="<?php echo $field->id; ?>"><?php echo $field->title; ?></label><?php } ?>
 <?php if($field->data['prefix']){ ?><span class="input-number-prefix"><?php echo $field->data['prefix']; ?></span><?php } ?>
-<?php echo html_input('text', $field->element_name, $value, $field->data['attributes']); ?>
+<?php echo html_input('number', $field->element_name, $value, $field->data['attributes']); ?>
 <?php if($field->data['units']){ ?><span class="input-number-units"><?php echo $field->data['units']; ?></span><?php } ?>
